76ee699af566033b0cc1f2f89ce27975811bdff1
Franek Kaminski <xeven770@gmail.com>
2026-02-26 10:07:04 +0100

    added more assets and math

--- models/character.class.js
+++ models/character.class.js
@@ -11,8 +11,28 @@ class Character extends MovableObject {
         "./img/2_character_pepe/2_walk/W-24.png",
         "./img/2_character_pepe/2_walk/W-25.png",
         "./img/2_character_pepe/2_walk/W-26.png"
+    ];
+    
+    IMAGES_DEAD = [
+        "img/2_character_pepe/5_dead/D-51.png",
+        "img/2_character_pepe/5_dead/D-52.png",
+        "img/2_character_pepe/5_dead/D-53.png",
+        "img/2_character_pepe/5_dead/D-54.png",
+        "img/2_character_pepe/5_dead/D-55.png",
+        "img/2_character_pepe/5_dead/D-56.png",
+        "img/2_character_pepe/5_dead/D-57.png",
+    ];
+
+    IMAGES_HURT = [
+        "img/2_character_pepe/4_hurt/H-41.png",
+        "img/2_character_pepe/4_hurt/H-42.png",
+        "img/2_character_pepe/4_hurt/H-43.png"
     ]
+    
     world;
+    walking_sound = new Audio("./audio/character/characterRun.mp3")
+
+
     IMAGES_JUMPING = [
         "./img/2_character_pepe/3_jump/J-31.png",
         "./img/2_character_pepe/3_jump/J-32.png",
@@ -23,12 +43,14 @@ class Character extends MovableObject {
         "./img/2_character_pepe/3_jump/J-37.png",
         "./img/2_character_pepe/3_jump/J-38.png",
         "./img/2_character_pepe/3_jump/J-39.png",
-    ]
+    ];
 
     constructor() {
         super().loadImage("./img/2_character_pepe/2_walk/W-21.png");
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_JUMPING);
+        this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_HURT);
         this.applyGravity();
         this.animate();
     }
@@ -57,10 +79,13 @@ class Character extends MovableObject {
 
         setInterval(() => {
 
-            if (this.IsAboveGround()) {
+            if (this.isDead()) {
+                this.playAnimation(this.IMAGES_DEAD);
+            } else if(this.isHurt()) {
+                this.playAnimation(this.IMAGES_HURT);
+            } else if (this.IsAboveGround()) {
                 this.playAnimation(this.IMAGES_JUMPING);
             } else {
-
                 if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
                     this.playAnimation(this.IMAGES_WALKING);
                 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,15 +1,10 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
+    energy = 100;
+    lastHit = 0;
 
     applyGravity() {
         setInterval(() => {
@@ -24,34 +19,64 @@ class MovableObject {
         return this.y < 180;
     }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
+    drawFrame(ctx) {
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = "3";
+            ctx.strokeStyle = "red";
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
     }
 
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-
+    isColliding(mo) {
+        return this.x + this.width > mo.x &&
+            this.y + this.height > mo.y &&
+            this.x < mo.x &&
+            this.y < mo.y + mo.height;
     }
+
+    hit() {
+        this.energy -= 5;
+        if (this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
+    }
+
+    isDead() {
+        return this.energy == 0;
+    }
+
+
+    //     if(character.x + character.width > chicken.x &&
+    //         character.y + character.height > chicken.y &&
+    //         character.x < chicken.x &&
+    //         character.y < chicken.y + chicken.height
+    // )
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
     }
 
-
     moveRight() {
         this.x += this.speed;
     }
+
     moveLeft() {
         this.x -= this.speed;
     }
+
     jump() {
         this.speedY = 30;
     }
